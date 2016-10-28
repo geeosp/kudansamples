a5cb1b00db8e9893c18ad9656721377c76374e1a
geeosp <geeo.sp@gmail.com>
2016-10-28 20:11:06 -0300

    initial commit

---
+++ app/src/main/java/eu/kudan/kudansamples/MainActivity.java
@@ -0,0 +1,15 @@
+package eu.kudan.kudansamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import eu.kudan.kudan.ARAPIKey;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ARAPIKey key = ARAPIKey.getInstance();
+        key.setAPIKey("GAWAE-FBVCC-XA8ST-GQVZV-93PQB-X7SBD-P6V4W-6RS9C-CQRLH-78YEU-385XP-T6MCG-2CNWB-YK8SR-8UUQ");
+    }
+}
