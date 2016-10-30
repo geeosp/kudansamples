fe4f05f9b6ffa5ea9d446c046c3375db9f99b0dd
Geovane Pereira <geeo.sp@gmail.com>
2016-10-30 02:16:15 -0300

    Image tracking working

--- app/src/main/java/com/voxar/arauthtool/TrackingActivity.java
+++ app/src/main/java/com/voxar/arauthtool/TrackingActivity.java
@@ -2,6 +2,8 @@ package com.voxar.arauthtool;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+
+
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,23 +15,29 @@ import eu.kudan.kudan.ARImageTrackable;
 import eu.kudan.kudan.ARImageTrackableListener;
 import eu.kudan.kudan.ARImageTracker;
 import eu.kudan.kudan.ARNode;
+import eu.kudan.kudan.ARVideoNode;
+import eu.kudan.kudan.ARVideoTexture;
 import eu.kudan.kudan.ARView;
 import eu.kudan.kudansamples.R;
 
 public class TrackingActivity extends ARActivity implements ARImageTrackableListener {
+    public String uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
+        uri =  getIntent().getExtras().getString("filepath");
+
     }
 
     @Override
     public void setup() {
-        //super.setup();
+        super.setup();
 
         ARImageTrackable waves = new ARImageTrackable("waves");
-        waves.loadFromAsset("waves.png");
+        //waves.loadFromAsset("waves.png");
+        waves.loadFromPath(uri);
         ARImageTracker tracker = ARImageTracker.getInstance();
         Log.i("geeo", "Initialized Tracker");
         tracker.addTrackable(waves);
@@ -37,16 +45,21 @@ public class TrackingActivity extends ARActivity implements ARImageTrackableList
 
 
 
-        ARImageTrackable legoTrackable = tracker.findTrackable("waves");
+      //  ARImageTrackable legoTrackable = tracker.findTrackable("waves");
         ARImageNode imageNode = new ARImageNode("waves.png");
+       // ARVideoTexture bugTexture = new ARVideoTexture();
 
+      //  bugTexture.loadFromAsset("bug.mp4");
+      //  ARVideoNode bug = new ARVideoNode(bugTexture);
+       // bug.preRender();
 
         // make it smaller.
-//        imageNode.scaleBy(0.5f, 0.5f, 0.5f);
+       //imageNode.scaleBy(0.5f, 0.5f, 0.5f);
 
         // add it to the lego trackable.
-        legoTrackable.getWorld().addChild(imageNode);
-        legoTrackable.getWorld().getFullTransform();
+        waves.getWorld().addChild(imageNode);
+    //    legoTrackable.getWorld().addChild(bug);
+        waves.getWorld().getFullTransform();
 
 
 
@@ -88,11 +101,41 @@ public class TrackingActivity extends ARActivity implements ARImageTrackableList
 
     @Override
     public void didTrack(ARImageTrackable trackable) {
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
 		Log.i("KudanSamples", "tracked"+ trackable.getName());
     }
 
     @Override
     public void didLose(ARImageTrackable trackable) {
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
         Log.i("KudanSamples", "lost " + trackable.getName());
     }
 }
--- app/src/main/java/eu/kudan/kudansamples/MainActivity.java
+++ app/src/main/java/eu/kudan/kudansamples/MainActivity.java
@@ -3,22 +3,28 @@ package eu.kudan.kudansamples;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.voxar.arauthtool.TrackingActivity;
 
 import eu.kudan.kudan.ARAPIKey;
 import eu.kudan.kudan.ARNode;
-
-
+import gun0912.tedbottompicker.TedBottomPicker;
 
 
 public class MainActivity extends AppCompatActivity {
- final int  MY_PERMISSION_CAMERA =1;
+    final int MY_PERMISSION_CAMERA = 1;
+    final int MY_PERMISSION_READ_EXTERNAL_STORAGE = 2;
+    final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +40,59 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-    //chamado quando oapp fica em evidencia;
+        //chamado quando oapp fica em evidencia;
         super.onResume();
-        openARActivity();
+        //openARActivity();
     }
 
-    protected void openARActivity(){
+    public void onButtonPressed(View v) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSION_WRITE_EXTERNAL_STORAGE);
+        } else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSION_CAMERA);
+        }
+
+
+
+
+
+        else {
+
+            TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(MainActivity.this)
+                    .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
+                        @Override
+                        public void onImageSelected(Uri uri) {
+                            Log.i("geeo",uri.getPath());
+                            Intent intent = new Intent(getApplicationContext(), TrackingActivity.class);
+                            intent.putExtra("filepath",uri.getPath());
+                            startActivity(intent);
+                        }
+                    })
+                    .create();
+
+            tedBottomPicker.show(getSupportFragmentManager());
+        }
+    }
+
+
+    protected void openARActivity() {
 
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this,
@@ -64,15 +117,11 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
-        }else{
-            Intent intent= new Intent(this, TrackingActivity.class);
+        } else {
+            Intent intent = new Intent(this, TrackingActivity.class);
             startActivity(intent);
 
         }
-
-
-
-
 
 
     }
@@ -80,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-                case MY_PERMISSION_CAMERA: {
+            case MY_PERMISSION_CAMERA: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -88,8 +137,9 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
 
-                  }
                 }
+            }
 
         }
-}}
+    }
+}
