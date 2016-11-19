3c81e41d5b5d3b6d653a52ba8c318575fdfeb3d5
Geovane Pereira <geeo.sp@gmail.com>
2016-11-18 23:26:30 -0300

    layout starting to get shape

---
+++ app/src/main/java/com/voxar/arauthtool/BookListActivity.java
@@ -0,0 +1,17 @@
+package com.voxar.arauthtool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+import eu.kudan.kudansamples.R;
+
+public class BookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+
+    }
+}
