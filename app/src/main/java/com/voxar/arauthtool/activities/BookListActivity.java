bdd7d26500cf57ebd79e1c20735e3188034944b1
Geovane Pereira <geeo.sp@gmail.com>
2016-12-06 20:04:19 -0300

    Consertando as permissoes

--- app/src/main/java/com/voxar/arauthtool/activities/BookListActivity.java
+++ app/src/main/java/com/voxar/arauthtool/activities/BookListActivity.java
@@ -1,7 +1,13 @@
 package com.voxar.arauthtool.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,11 +28,13 @@ import eu.kudan.kudansamples.MyApplication;
 import eu.kudan.kudansamples.R;
 
 public class BookListActivity extends AppCompatActivity {
+    final int MY_PERMISSION_CAMERA = 1;
+    final int MY_PERMISSION_READ_EXTERNAL_STORAGE = 2;
+    final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 3;
     BookDatabase database;
     List<Book> books;
     RecyclerView recyclerView;
     BookAdapter bookAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +59,58 @@ public class BookListActivity extends AppCompatActivity {
         //RecyclerView.LayoutManager layout = new GridLayoutManager(this,2);
 
         recyclerView.setLayoutManager(layout);
-
+        permissionsRequest();
 
     }
+
+
+    // Requests app permissions
+    public void permissionsRequest() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 111);
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 111: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    permissionsNotSelected();
+                }
+            }
+        }
+    }
+
+    private void permissionsNotSelected() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissions Requred");
+        builder.setMessage("Please enable the requested permissions in the app settings in order to use this demo app");
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+                System.exit(1);
+            }
+        });
+        AlertDialog noInternet = builder.create();
+        noInternet.show();
+    }
+
 
     @Override
     protected void onPostResume() {
