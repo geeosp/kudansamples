1e95146ec5f0c0ab23875f7c99d48b8f4f18cbc3
Geovane Pereira <geeo.sp@gmail.com>
2016-12-09 20:24:06 -0300

    Testando arquivos

--- app/src/main/java/com/voxar/arauthtool/tests/FileTest.java
+++ app/src/main/java/com/voxar/arauthtool/tests/FileTest.java
@@ -1,13 +1,22 @@
 package com.voxar.arauthtool.tests;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
 
 import com.voxar.arauthtool.models.Book;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.List;
 
@@ -44,7 +53,7 @@ public class FileTest extends AppCompatActivity {
         textView.setText(json);
     }
 
-    public void pickFile(View v){
+    public void pickFile(View v) {
         FileChooserDialog.Builder builder = new FileChooserDialog.Builder(FileChooserDialog.ChooserType.FILE_CHOOSER, new FileChooserDialog.ChooserListener() {
             @Override
             public void onSelect(String path) {
@@ -58,5 +67,65 @@ public class FileTest extends AppCompatActivity {
         }
     }
 
+    public void copy(View v) {
+        String path = textView.getText().toString();
 
+        String extension = path.substring(path.lastIndexOf('.'));
+        Log.d("SavingBook", "lesson Extension: " + extension);
+        File oldFile = new File(path);
+        File newFile = new File(getApplicationContext().getFilesDir(), "" + System.currentTimeMillis() + extension);
+        String newPath = newFile.getPath();
+        Log.d("SavingBook", "newFilePath: " + newPath);
+        try {
+            if (newFile.exists()) {
+                newFile.delete();
+                newFile.createNewFile();
+
+            }
+            copy(oldFile, newFile);
+            Log.e("SavingBook", "Sucessfull" + newFile.getPath());
+            textView.setText(newFile.getPath());
+        } catch (IOException e) {
+            Log.e("SavingBook", "Not Sucessfull" + e.getMessage());
+        }
+
+
+    }
+
+
+    void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
+    public void openInternFile(View v){
+        String path= textView.getText().toString();
+        File temp_file=new File(path);
+        Intent intent = new Intent();
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(Uri.fromFile(temp_file),getMimeType(temp_file.getAbsolutePath()));
+        startActivity(intent);
+
+    }
+    private String getMimeType(String url)
+    {
+        String parts[]=url.split("\\.");
+        String extension=parts[parts.length-1];
+        String type = null;
+        if (extension != null) {
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            type = mime.getMimeTypeFromExtension(extension);
+        }
+        return type;
+    }
 }
